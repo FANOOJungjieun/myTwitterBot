fed1dabaeae1838be936dbc6289ceb4529e9cb04
FANOOJungjieun <jbh1657@naver.com>
2022-06-02 16:08:07 +0900

    template

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("test");
+import tweepy
+
+API_KEY = ""
+API_KEY_SECRET = ""
+
+ACCESS_TOKEN = ""
+ACCESS_SECRET = ""
+
+#발급받은 key token 입력
+
+auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET, callback='oob') 
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+# 4.퍼블릭 트윗 작성
+api.update_status("테스트...(해킹아님)")
