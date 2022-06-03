48bb474a2c79b2f58c8ce873ab438be8d6ccb6ad
FANOOJungjieun <jbh1657@naver.com>
2022-06-03 17:57:42 +0900

    다른 계정 권한 받기

--- test.py
+++ test.py
@@ -3,14 +3,30 @@ import tweepy
 API_KEY = ""
 API_KEY_SECRET = ""
 
-ACCESS_TOKEN = ""
-ACCESS_SECRET = ""
-
-#발급받은 key token 입력
+#my ACCESS_TOKEN
+#ACCESS_TOKEN = ""
+#ACCESS_SECRET = ""
 
 auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET, callback='oob') 
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+try: # 성공 했을 때
+    redirect_url = auth.get_authorization_url() 
+    print(redirect_url)
+except tweepy.TweepError: # 에러 떴을 때 
+    print('리퀘스트 토큰을 가져오는 데에 실패했습니다.')
+
+
+# 핀 번호 저장 및 세팅
+pin_number = input("핀 번호:")
+auth.get_access_token(pin_number)
+
+#사용자의 토큰을 세팅, Twitter API wrapper 생성
+USER_ACCESS_TOKEN = auth.access_token
+USER_ACCESS_SECRET= auth.access_token_secret
+auth.set_access_token(USER_ACCESS_TOKEN, USER_ACCESS_SECRET)
+print(USER_ACCESS_TOKEN)
+print(USER_ACCESS_SECRET)
 api = tweepy.API(auth)
 
-# 4.퍼블릭 트윗 작성
-api.update_status("테스트...(해킹아님)")
+#트윗하기 
+api.update_status("다른 계정 트윗 작성")
