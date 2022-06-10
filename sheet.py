5c8b72839ae2097f49df8a7e2207dc21f7d45cb4
FANOOJungjieun <jbh1657@naver.com>
2022-06-10 17:26:19 +0900

    시트 업데이트

--- sheet.py
+++ sheet.py
@@ -1,10 +1,11 @@
 import gspread
 import pprint
 
+# https://docs.google.com/spreadsheets/d/1mMF5-mvGgDFztfzrJFkjYNj0l1w3YpVUxnO2a1FlUys/edit#gid=0
 
-gc = gspread.service_account(filename='-.json') #json파일 이름
+gc = gspread.service_account(filename='my-twitter-bot-352609-f7d7849f8ef1.json') #json파일 이름
 
-wks = gc.open("-") #시트 이름
+wks = gc.open("테스트 시트") #시트 이름
 
 # 시트 탭 선택 함수
 def select_sheet(sheet_name):
@@ -13,7 +14,7 @@ def select_sheet(sheet_name):
 
 # 시트 전체 정보 터미널에 프린트하는 함수
 def shop_sheet(worksheet):    
-    all_shop_info= worksheet.get_values()
+    all_shop_info= worksheet.get_all_records()
     pprint.pprint(all_shop_info)
     
 
